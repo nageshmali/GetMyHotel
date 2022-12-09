bf3231751787ed21f7b2780ab5013e6835fd0c84
Nagesh Mali <malinagesh60@gmail.com>
2022-12-10 00:27:16 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 
 
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
       <>
         <Router>
+          <Header/>
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/profile" element={<Profile/>} />
